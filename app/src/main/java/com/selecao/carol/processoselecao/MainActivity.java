00b1100af42b4d0c189f1986a70c1133e2f82d00
Carol <krodrigies@gmail.com>
2017-02-18 14:32:29 -0400

    Configuracao do projeto

---
+++ app/src/main/java/com/selecao/carol/processoselecao/MainActivity.java
@@ -0,0 +1,13 @@
+package com.selecao.carol.processoselecao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
