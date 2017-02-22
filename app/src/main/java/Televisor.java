6fec9179329f6bfe72189916153da29617298d7f
Carol <krodrigies@gmail.com>
2017-02-21 20:48:36 -0400

    adicionei a classe Tv

---
+++ app/src/main/java/Televisor.java
@@ -0,0 +1,22 @@
+/**
+ * Created by willian on 21/02/17.
+ */
+
+public class Televisor {
+
+    private String descricao;
+    private double valorTV;
+    private String urlImagem;
+
+    public String getDescricao () { return descricao; }
+
+    public void setDescricao (String descricao) { this.descricao = descricao; }
+
+    public double getValorTV (){ return valorTV; }
+
+    public void setValorTV (double valorTV) { this.valorTV = valorTV; }
+
+    public String getUrlImagem () { return urlImagem; }
+
+    public void setUrlImagem ( String urlImagem ) { this.urlImagem = urlImagem; }
+}
